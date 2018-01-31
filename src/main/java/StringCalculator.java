42806bb1505786fcea2d5a30590199216442738e
Rafał <catamenia91@gmail.com>
2018-01-31 19:44:59 +0100

    implemented first point

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+
 public class StringCalculator {
 
+
     public int Add(String numbers) {
+        if(numbers.length() > 1) {
+            int sum = 0;
+            int[] intArray = Arrays.asList(numbers.split(",")).stream().mapToInt(Integer::parseInt).toArray();
+            for (int e : intArray) {
+                sum = sum + e;
+            }
+            return sum;
+        }
+        if(numbers.length() == 1){
+            return Integer.parseInt(numbers);
+        }
         return 0;
     }
 }
