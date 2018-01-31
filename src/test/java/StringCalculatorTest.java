87891f9c12da15fe1d10d0c1c6e04fd4b9d68844
Rafał <catamenia91@gmail.com>
2018-01-31 20:44:09 +0100

    changed tests into one test with params

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,54 +1,76 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 
 /**
  * Tdd training on 31.01.18.
  */
-
+@RunWith(JUnitParamsRunner.class)
 public class StringCalculatorTest {
 
+//    @Test
+//    public void shouldCreateObject() {
+//        StringCalculator testedObject = new StringCalculator();
+//        assertThat(testedObject).isNotNull();
+//        assertThat(testedObject.Add("")).isEqualTo(0);
+//    }
+//
+//    @Test
+//    public void shouldReturnOneWhenOnePassed() {
+//        StringCalculator testedObject = new StringCalculator();
+//        int result = testedObject.Add("1");
+//        assertThat(result).isEqualTo(1);
+//    }
+//
+//    @Test
+//    public void shouldRturnTwoWhenOneAndOnePassed() {
+//        StringCalculator testedObject = new StringCalculator();
+//        int result = testedObject.Add("1,1");
+//        assertThat(result).isEqualTo(2);
+//    }
+//
+//    @Test
+//    public void shouldRturnTwentyOneWhenOneAndTwentyPassed() {
+//        StringCalculator testedObject = new StringCalculator();
+//        int result = testedObject.Add("1,20");
+//        assertThat(result).isEqualTo(21);
+//    }
+//
+//    @Test
+//    public void shouldRturnTwoThousandWhenThousandAndThousandPassed() {
+//        StringCalculator testedObject = new StringCalculator();
+//        int result = testedObject.Add("1000,1000");
+//        assertThat(result).isEqualTo(2000);
+//    }
+//
+//    @Test
+//    public void shouldRturnThreeWhenOneAndOneAndOnePassed() {
+//        StringCalculator testedObject = new StringCalculator();
+//        int result = testedObject.Add("1,1,1");
+//        assertThat(result).isEqualTo(3);
+//    }
+
     @Test
-    public void shouldCreateObject() throws Exception {
+    @Parameters(method = "paramsForTest")
+    public void shouldWorksWithParametrizedTest(String input, int expectedResult){
         StringCalculator testedObject = new StringCalculator();
-        assertThat(testedObject).isNotNull();
-        assertThat(testedObject.Add("")).isEqualTo(0);
+        int result = testedObject.Add(input);
+        assertThat(result).isEqualTo(expectedResult);
     }
 
-    @Test
-    public void shouldRturnOneWhenOnePassed() {
-        StringCalculator testedObject = new StringCalculator();
-        int result = testedObject.Add("1");
-        assertThat(result).isEqualTo(1);
-    }
 
-    @Test
-    public void shouldRturnTwoWhenOneAndOnePassed() {
-        StringCalculator testedObject = new StringCalculator();
-        int result = testedObject.Add("1,1");
-        assertThat(result).isEqualTo(2);
-    }
-
-    @Test
-    public void shouldRturnTwentyOneWhenOneAndTwentyPassed() {
-        StringCalculator testedObject = new StringCalculator();
-        int result = testedObject.Add("1,20");
-        assertThat(result).isEqualTo(21);
-    }
-
-    @Test
-    public void shouldRturnTwoThousandWhenThousandAndThousandPassed() {
-        StringCalculator testedObject = new StringCalculator();
-        int result = testedObject.Add("1000,1000");
-        assertThat(result).isEqualTo(2000);
-    }
-
-    @Test
-    public void shouldRturnThreeWhenOneAndOneAndOnePassed() {
-        StringCalculator testedObject = new StringCalculator();
-        int result = testedObject.Add("1,1,1");
-        assertThat(result).isEqualTo(3);
-
+    private Object[][] paramsForTest(){
+        return new Object[][]{
+                {"", 0},
+                {"1",1},
+                {"1,1",2},
+                {"1,20,",21},
+                {"1,20,",21},
+                {"1000,1000,",2000},
+                {"1,1,1",3}
+        };
     }
 }
