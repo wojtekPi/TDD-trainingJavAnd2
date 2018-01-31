c9333f44c7556dce1c21d34ea64108ad4ca7d1e3
Rafał <catamenia91@gmail.com>
2018-01-31 20:53:22 +0100

    deleted comments from StringCalculatorTest

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -11,48 +11,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @RunWith(JUnitParamsRunner.class)
 public class StringCalculatorTest {
 
-//    @Test
-//    public void shouldCreateObject() {
-//        StringCalculator testedObject = new StringCalculator();
-//        assertThat(testedObject).isNotNull();
-//        assertThat(testedObject.Add("")).isEqualTo(0);
-//    }
-//
-//    @Test
-//    public void shouldReturnOneWhenOnePassed() {
-//        StringCalculator testedObject = new StringCalculator();
-//        int result = testedObject.Add("1");
-//        assertThat(result).isEqualTo(1);
-//    }
-//
-//    @Test
-//    public void shouldRturnTwoWhenOneAndOnePassed() {
-//        StringCalculator testedObject = new StringCalculator();
-//        int result = testedObject.Add("1,1");
-//        assertThat(result).isEqualTo(2);
-//    }
-//
-//    @Test
-//    public void shouldRturnTwentyOneWhenOneAndTwentyPassed() {
-//        StringCalculator testedObject = new StringCalculator();
-//        int result = testedObject.Add("1,20");
-//        assertThat(result).isEqualTo(21);
-//    }
-//
-//    @Test
-//    public void shouldRturnTwoThousandWhenThousandAndThousandPassed() {
-//        StringCalculator testedObject = new StringCalculator();
-//        int result = testedObject.Add("1000,1000");
-//        assertThat(result).isEqualTo(2000);
-//    }
-//
-//    @Test
-//    public void shouldRturnThreeWhenOneAndOneAndOnePassed() {
-//        StringCalculator testedObject = new StringCalculator();
-//        int result = testedObject.Add("1,1,1");
-//        assertThat(result).isEqualTo(3);
-//    }
-
     @Test
     @Parameters(method = "paramsForTest")
     public void shouldWorksWithParametrizedTest(String input, int expectedResult){
