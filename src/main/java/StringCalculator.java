d14c79e1d3318316ea6309fd24cc58dcc692dcb3
Rafał <catamenia91@gmail.com>
2018-01-31 19:56:05 +0100

    changed one if from add method

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -4,16 +4,13 @@ public class StringCalculator {
 
 
     public int Add(String numbers) {
-        if(numbers.length() > 1) {
+        if(numbers.length() > 0) {
             int sum = 0;
             int[] intArray = Arrays.asList(numbers.split(",")).stream().mapToInt(Integer::parseInt).toArray();
             for (int e : intArray) {
                 sum = sum + e;
             }
             return sum;
-        }
-        if(numbers.length() == 1){
-            return Integer.parseInt(numbers);
         }
         return 0;
     }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -17,31 +17,38 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void shouldRturnOneWhenOnePassed(){
+    public void shouldRturnOneWhenOnePassed() {
         StringCalculator testedObject = new StringCalculator();
         int result = testedObject.Add("1");
         assertThat(result).isEqualTo(1);
     }
+
     @Test
-    public void shouldRturnTwoWhenOneAndOnePassed(){
+    public void shouldRturnTwoWhenOneAndOnePassed() {
         StringCalculator testedObject = new StringCalculator();
         int result = testedObject.Add("1,1");
         assertThat(result).isEqualTo(2);
     }
 
     @Test
-    public void shouldRturnTwentyOneWhenOneAndTwentyPassed(){
+    public void shouldRturnTwentyOneWhenOneAndTwentyPassed() {
         StringCalculator testedObject = new StringCalculator();
         int result = testedObject.Add("1,20");
         assertThat(result).isEqualTo(21);
     }
 
     @Test
-    public void shouldRturnTwoThousandWhenThousandAndThousandPassed(){
+    public void shouldRturnTwoThousandWhenThousandAndThousandPassed() {
         StringCalculator testedObject = new StringCalculator();
         int result = testedObject.Add("1000,1000");
         assertThat(result).isEqualTo(2000);
     }
 
+    @Test
+    public void shouldRturnThreeWhenOneAndOneAndOnePassed() {
+        StringCalculator testedObject = new StringCalculator();
+        int result = testedObject.Add("1,1,1");
+        assertThat(result).isEqualTo(3);
 
+    }
 }
