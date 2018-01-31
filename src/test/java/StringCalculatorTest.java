d76b5a5eafc0dcadf299e97ed0994e18f9535e7a
dwojtowicz <dan.wojtowicz@wp.pl>
2018-01-31 20:46:01 +0100

    AddedParametrTest

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,4 +1,7 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -6,49 +9,27 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 /**
  * Tdd training on 31.01.18.
  */
-
+@RunWith(JUnitParamsRunner.class)
 public class StringCalculatorTest {
 
-    @Test
-    public void shouldCreateObject() throws Exception {
-        StringCalculator testedObject = new StringCalculator();
-        assertThat(testedObject).isNotNull();
-        assertThat(testedObject.Add("")).isEqualTo(0);
+    private Object[][] paramsForTests(){
+        return new Object[][]{
+                {"1",1},
+                {"1,1", 2},
+                {"1000,1000", 2000},
+                {"1,1,1", 3},
+                {"25,25,25", 75},
+                {"", 0}
+        };
     }
 
     @Test
-    public void shouldRturnOneWhenOnePassed() {
+    @Parameters(method = "paramsForTests")
+    public void shouldWorksWithParametrizedTest(String input, int expectedResults){
         StringCalculator testedObject = new StringCalculator();
-        int result = testedObject.Add("1");
-        assertThat(result).isEqualTo(1);
-    }
 
-    @Test
-    public void shouldRturnTwoWhenOneAndOnePassed() {
-        StringCalculator testedObject = new StringCalculator();
-        int result = testedObject.Add("1,1");
-        assertThat(result).isEqualTo(2);
-    }
-
-    @Test
-    public void shouldRturnTwentyOneWhenOneAndTwentyPassed() {
-        StringCalculator testedObject = new StringCalculator();
-        int result = testedObject.Add("1,20");
-        assertThat(result).isEqualTo(21);
-    }
-
-    @Test
-    public void shouldRturnTwoThousandWhenThousandAndThousandPassed() {
-        StringCalculator testedObject = new StringCalculator();
-        int result = testedObject.Add("1000,1000");
-        assertThat(result).isEqualTo(2000);
-    }
-
-    @Test
-    public void shouldRturnThreeWhenOneAndOneAndOnePassed() {
-        StringCalculator testedObject = new StringCalculator();
-        int result = testedObject.Add("1,1,1");
-        assertThat(result).isEqualTo(3);
+        int result = testedObject.Add(input);
+        assertThat(result).isEqualTo(expectedResults);
 
     }
 }
